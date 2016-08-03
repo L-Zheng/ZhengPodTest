a070324421dae285832433a3f2d7dcdc1264602a
李保征 <418736259@qq.com>
2016-08-03 10:40:26 +0800

    添加开发调试工具,创建podspec文件

---
+++ DevelopmentTool/DevelopmentTool.h
@@ -0,0 +1,34 @@
+//
+//  DevelopmentTool.h
+//  GoldTouTiao
+//
+//  Created by 李保征 on 16/7/22.
+//  Copyright © 2016年 wallstreetcn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DevelopmentTool : NSObject
+
+/** 设置 正式or测试 网络地址  */
++ (void)setupNetWorkEnvironment;
+
+/** 工程配置环境 调试or发布环境 */
++ (BOOL)isDebugPrjConfig;
+
+/** 是否第一次安装app */
++ (BOOL)isFirstSetupApp;
+
+/** 是否是 调试地址 */
++ (BOOL)isDebugNetWork;
+
+/** 设置正式地址 */
++ (BOOL)setReleaseNetWork;
+
+/** 设置测试地址 */
++ (BOOL)setDebugNetWork;
+
+/** 请求网络地址 */
++ (NSString *)baseUrl;
+
+@end
